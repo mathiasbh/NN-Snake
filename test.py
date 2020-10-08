0b46bf7f232d5dd182b25b99d399f36b233d73ef
Mathias Bojsen-Hansen <mathiassaih@gmail.com>
2020-10-08 11:07:42 +0200

    new run

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ from utils import run_game
 from nn import *
 from SnakeGame import *
 
-height = 400
-width = 400
+height = 200
+width = 200
 
 # Load model
 model = NeuralNetwork()
 model.build((None, 4))
 model(np.array([[0,0,0,0]]))
-model.load_weights('__models/checkpoint_restart_generation1073')
+model.load_weights('__models/04102020_10snakes_simplevision/checkpoint_generation989')
 
 game = SnakeGame(render=True, player=False, FPS=60, height=height, width=width)
 run_game(game, model, replay=True)
--- train.py
+++ train.py
@@ -7,18 +7,21 @@ height = 200
 max_generations = 1000
 population_size = 200
 mutation_rate = 0.01
-games_per_snake = 10
+games_per_snake = 20
+distance_option = "simple" 
+# setting this to anything other than "simple" changes distance option to measuring actual distance to nearest wall rather than boolean value if there is a wall
+# or snake segment on the square right next the the snake head.
 
 
 if __name__ == '__main__':
     # Initialize models and snakegames
     models = np.array([])
-    game = SnakeGame(render=False, player=False, width=width, height=height)
+    game = SnakeGame(render=False, player=False, width=width, height=height, distance_option=distance_option)
     for m in range(population_size):
         models = np.append(models, NeuralNetwork())
 
     for generation in range(max_generations):
-        models, fitnessGeneration, scoreGeneration = train_step(game, models, mutation_rate, generation, population_size, max_generations, games_per_snake)
+        models, fitnessGeneration, scoreGeneration = train_step(game, models, mutation_rate, generation, population_size, max_generations, games_per_snake, fitness_option='best')
         
         fitnessAvg = np.mean(fitnessGeneration)
         fitnessMax = np.max(fitnessGeneration)
